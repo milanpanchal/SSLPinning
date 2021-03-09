503156f2fec456633d0dcec36c252c0ee2ce72c8
Milan Panchal <sam07it22@gmail.com>
2021-03-09 13:35:11 +0530

    Initial Commit

---
+++ SSLPinning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SSLPinning
+//
+//  Created by Milan Panchal on 09/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
